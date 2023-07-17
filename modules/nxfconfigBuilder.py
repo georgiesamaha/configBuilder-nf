0a15f73a24bceef7bb1b9cfe97b7cb18886106b2
georgiesamaha <georgina.samaha@sydney.edu.au>
2023-07-17 13:32:01 +1000

    Add documentation links to custom nxf prompts

--- modules/nxfconfigBuilder.py
+++ modules/nxfconfigBuilder.py
@@ -26,36 +26,21 @@ def nxfconfigBuilder():
     module = 'nextflow'
     clusterOptions = ''
 
-    executor = input(Fore.MAGENTA + "What executor does your system use (default is 'local')? " + Style.RESET_ALL)
+    executor = input(Fore.MAGENTA + "What executor does your system use (default is 'local')? \nFor more information, see https://www.nextflow.io/docs/latest/executor.html \n" + Style.RESET_ALL)
     executor = executor if executor else 'local'
 
-    # Nextflow supported job schedulers: https://www.nextflow.io/docs/latest/executor.html 
     if executor in ['pbspro', 'slurm', 'azure batch', 'aws batch', 'bridge', 'flux', 'lsf', 'moab', 'oar', 'nqsii', 'pbs', 'sge']:
-        
-        # Enter queue to run processes on
-        queue = input(Fore.MAGENTA + "What queue would you like to run your jobs on? " + Style.RESET_ALL)
-    
-        # Define modules to run before executing workflow processes 
-        module = input(Fore.MAGENTA + "What modules and versions should be loaded before executing each process (default is 'nextflow')? " + Style.RESET_ALL)
+        queue = input(Fore.MAGENTA + "What queue would you like to run your jobs on? For more information, see https://www.nextflow.io/docs/latest/process.html#queue \n" + Style.RESET_ALL)
+        module = input(Fore.MAGENTA + "What modules and versions should be loaded before executing each process (default is 'nextflow')? \nFor more information, see https://www.nextflow.io/docs/latest/process.html#beforescript \n" + Style.RESET_ALL)
         module = module if module else 'nextflow'
+        clusterOptions = input(Fore.MAGENTA + "What cluster options should be applied to each process execution (e.g. -P <projectname>)? \nFor more information, see https://www.nextflow.io/docs/latest/process.html#clusteroptions \n" + Style.RESET_ALL)
 
-        # Define any cluster options that need to be applied 
-        clusterOptions = input(Fore.MAGENTA + "What cluster options should be applied to each process execution (e.g. -P <projectname>)? " + Style.RESET_ALL)
-
-    # Enter number of CPUs available on the queue
-    cpus = ask_for_input(Fore.MAGENTA + "What is the max number of CPUs available on this queue (default is 1)? ", type_=int, min_=1)
+    cpus = ask_for_input(Fore.MAGENTA + "What is the max number of CPUs available on this queue (default is 1)? \nFor more information, see https://www.nextflow.io/docs/latest/process.html#cpus \n", type_=int, min_=1)
     cpus = cpus if cpus else 1
-
-    # Enter the amount of memory available on the queue 
-    memory = ask_for_input(Fore.MAGENTA + "What is the max amount of memory in GB available on this queue (default is 1)? ", type_=int, min_=1)
+    memory = ask_for_input(Fore.MAGENTA + "What is the max amount of memory in GB available on this queue (default is 1)? \nFor more information, see https://www.nextflow.io/docs/latest/process.html#memory \n", type_=int, min_=1)
     memory = memory if memory else 1
-
-    # Enter the maxiumum walltime for the queue 
-    # See: https://www.nextflow.io/docs/latest/process.html?highlight=cpus#time
-    walltime = ask_for_input(Fore.MAGENTA + "What is the max walltime available on this queue (default is 0.5h)? ", type_=float, min_=0.5)
+    walltime = ask_for_input(Fore.MAGENTA + "What is the max walltime available on this queue (default is 0.5h)? \nFor more information, see https://www.nextflow.io/docs/latest/process.html#time \n", type_=float, min_=0.5)
     walltime = walltime if walltime else 0.5
-    
-    # Name custom config file
     output_file = input(Fore.YELLOW + "Enter the output file name (default is 'custom_nextflow.config'): " + Style.RESET_ALL)
     output_file = output_file if output_file else 'custom_nextflow.config'
 
@@ -73,4 +58,3 @@ def nxfconfigBuilder():
         f.write(f"    memory = '{memory}GB'\n")
         f.write(f"    time = '{walltime}h'\n")
         f.write("}\n\n")
-
