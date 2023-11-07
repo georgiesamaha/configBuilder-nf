52ee39815f9cfda1dadb380ad3772c5240a16d2f
James Fellows Yates <jfy133@gmail.com>
2023-11-07 06:11:21 +0100

    Restructure pipeline config question flow

--- modules/__init__.py
+++ modules/__init__.py
@@ -1,4 +1,4 @@
 # modules/__init__.py
 from .create_infrastructure import create_infrastructure
-from .create_nfcore import nfconfigBuilder
+from .create_pipeline import create_pipeline
 from .create_hpc_env import check_scheduler
