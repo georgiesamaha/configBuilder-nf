b440423ca56d679a1a85a64284d743f99ee88f63
James A. Fellows Yates <jfy133@gmail.com>
2024-01-11 07:36:24 +0100

    Start work on cachedir autodetection and question

--- modules/create_container.py
+++ modules/create_container.py
@@ -4,7 +4,7 @@ import subprocess
 import os
 import inquirer
 from colorama import Fore, Style, init
-from .utilities import create_tool_options
+from .utilities import create_tool_options, create_cachedir_options
 
 ## TODO: consider how to address `module` environment detection here
 
@@ -19,6 +19,13 @@ nxf_software_management = (
     "shifter",
 )
 
+nxf_software_cachedirs = [
+    "NXF_CHARLIECLOUD_CACHEDIR",
+    "NXF_CONDA_CACHEDIR",
+    "NXF_SINGULARITY_CACHEDIR",
+    "NXF_APPTAINER_CACHEDIR",
+]
+
 
 ## TODO: extend to instead use module detection system if available
 def detect_containers(options=nxf_software_management):
@@ -30,7 +37,20 @@ def detect_containers(options=nxf_software_management):
 container_options = detect_containers()
 
 
-def create_container_scope(options=container_options):
+def detect_cachedirs(options=nxf_software_cachedirs):
+    cachedir_options = create_cachedir_options(options)
+
+    return cachedir_options
+
+
+cachedir_options = detect_cachedirs()
+
+print(cachedir_options)
+
+
+def create_container_scope(
+    cont_options=container_options, cach_options=cachedir_options
+):
     if not container_options:
         print(Fore.YELLOW + "...no software environment systems detected.\n")
         question = [
@@ -63,9 +83,12 @@ def create_container_scope(options=container_options):
             inquirer.List(
                 "container_options",
                 message="Which software environment system would you like to use?",
-                choices=options,
+                choices=cont_options,
             )
         ]
         answer = inquirer.prompt(question)
+
+    print(cach_options)
+    cach_options
 
     return answer
--- modules/utilities.py
+++ modules/utilities.py
@@ -16,6 +16,15 @@ def check_tool_on_path(tool):
     return result
 
 
+def check_var_in_env(var):
+    """
+    Simply checks that an variable exists in the environment
+    """
+    status = os.getenv(var)
+    result = status  ## 127 means not found, everything else implies exists, we make assumption it's working
+    return result
+
+
 def create_tool_options(options):
     """
     Produces a list of 'valid' entries in a set, i.e., when running check_tool_on_path it will keep everything that has been evaluated to true.
@@ -25,5 +34,18 @@ def create_tool_options(options):
     available = set()
     for i in options:
         if check_tool_on_path(i):
+            available.add(i)
+    return available
+
+
+def create_cachedir_options(options):
+    """
+    Produces a list of 'valid' entries in a set, i.e., when running check_tool_on_path it will keep everything that has been evaluated to true.
+
+    This reduced list can then be used to offer 'valid' options to the user.
+    """
+    available = set()
+    for i in options:
+        if check_var_in_env(i):
             available.add(i)
     return available
